e1a686c2d349098d254e6a2ee35d4c52df1d05e8
SANTHOSH SJ <sarathisanthosh@gmail.com>
2020-03-29 12:21:30 +0530

    added  live reloader to reload browser when css changes

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ run npm install
 on terminal run : npx gulp
 ```
 
-### added autoprefixer
+###  autoprefixer
 we can write our CSS rules without vendor prefixes
 It will add automatically
+
+###  browser sync
+
+For auto re-loading webpage when file change
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require("gulp");
 const sass = require('gulp-sass');
 const autoprefixer = require("gulp-autoprefixer"); // to make css compatible for all browsers. see npm for example 
+const browserSync = require("browser-sync").create();
 
 gulp.task("default", async () => {
   // code for your default task goes here
@@ -8,11 +9,15 @@ gulp.task("default", async () => {
   // watch for file change and rebuild
   gulp.watch("./sass/**/*.scss", gulp.series('sass'));
 
+  // starts the server
+  browserSync.init({
+    server: "./"
+  });
+
 });
 
 
 gulp.task("sass", async () => {
-  console.log("inside gulp sass");
   gulp
     .src("./sass/**/*.scss") // all saas files inside saas folder and saas sub-folders
     .pipe(sass().on("error", sass.logError)) // converts file from saas to proper css; on error log error and coninue instead of breaking
@@ -22,7 +27,8 @@ gulp.task("sass", async () => {
       ]
     })
     )
-    .pipe(gulp.dest("./css"));
+    .pipe(gulp.dest("./css"))
+    .pipe(browserSync.stream()); //  reload at specific points during our tasks; here -when css changed reload
 });
 
 // gulp.watch("./sass/**/*.scss", ["sass"]);
